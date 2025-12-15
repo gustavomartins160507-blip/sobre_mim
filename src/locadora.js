818ed60b1fb8990fe8ecd9f537e1d655f6a17557
Gustavo M sousa <gustavomartins160507@gmail.com>
2025-12-14 23:06:38 -0300

    atualizações andamento do site

--- src/locadora.js
+++ src/locadora.js
@@ -1,9 +1,10 @@
-const botao = document.getElementById('side-bar-button')
-const aba = document.getElementById('aba')
-const btntoggle = document.getElementById('btn-toggle')
-const body = document.getElementById('body')
-const senhapass = document.getElementById('senha')
-const btnsenha = document.getElementById('btn-senha')
+const botao=document.getElementById('side-bar-button')
+const aba=document.getElementById('aba')
+const btntoggle=document.getElementById('btn-toggle')
+const body=document.getElementById('body')
+const senhapass=document.getElementById('senha')
+const btnsenha=document.getElementById('btn-senha')
+const main_content=document.getElementById('main-content')
 function sidebar(){
   if (aba && botao) {
     aba.classList.toggle('mostrar-aba')
@@ -18,6 +19,8 @@ function mudar_tema(){
     btntoggle.classList.replace('bi-toggle-on','bi-toggle-off')
   }
   body.classList.toggle('ativa1')
+  main_content.classList.toggle('ativa2')
+
 }
 function mostrar_senha(){
   if (!senhapass || !btnsenha) return
@@ -30,3 +33,16 @@ function mostrar_senha(){
     btnsenha.classList.replace('bi-eye-slash','bi-eye')
   }
 }
+const top_button = document.getElementById('top-button')
+const pixel_aparecer = 300
+
+function verificarScroll() {
+  if (window.scrollY > pixel_aparecer) {
+    top_button.classList.add('mostrar')
+  } else {
+    top_button.classList.remove('mostrar')
+  }
+}
+
+window.addEventListener('scroll', verificarScroll)
+window.addEventListener('load', verificarScroll)
